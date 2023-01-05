136cf71274f069a686e603edc4f3d66d04a7ad3e
Andrew Block <andy.block@gmail.com>
2023-01-05 05:05:19 -0600

    Reorg all charts
    Signed-off-by: Andrew Block <andy.block@gmail.com>

---
+++ charts/telescope-backend/files/db_tables.sql
@@ -0,0 +1,54 @@
+-- 
+-- DROP TABLE IF EXISTS domain;
+CREATE TABLE IF NOT EXISTS domain (
+  id SERIAL PRIMARY KEY,
+  description varchar(128),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+INSERT INTO domain(description) SELECT 'Secure Infrastructure' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Infrastructure');
+INSERT INTO domain(description) SELECT 'Secure Data' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Data');
+INSERT INTO domain(description) SELECT 'Secure Code' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Code');
+INSERT INTO domain(description) SELECT 'Secure Integrations' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Integrations');
+INSERT INTO domain(description) SELECT 'Secure Monitoring and Logging' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Monitoring and Logging');
+
+-- 
+-- DROP TABLE capability;
+
+CREATE TABLE IF NOT EXISTS capability (
+  id SERIAL PRIMARY KEY,
+  domain_id int,
+  flag_id int,
+  description varchar(128),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO capability(domain_id, description, flag_id) SELECT 1, 'Platform Hardening', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 1 AND description = 'Secure Infrastructure' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 1, 'Container Protection',1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 1 AND description = 'Container Protection' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 1, 'Secure Images', 2 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 1 AND description = 'Secure Images' AND flag_id = 2);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 2, 'Classification', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Classification' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 2, 'Vulnerability Scanning', 2 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Vulnerability Scanning' AND flag_id = 2);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 2, 'Encryption', 2 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Encryption' AND flag_id = 2);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 2, 'Loss Prevention', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Loss Prevention' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 3, 'Static Code Analysis', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Static Code Analysis' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 3, 'Dynamic Code Analysis', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 2 AND description = 'Dynamic Code Analysis' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 3, 'Vulnerability Assessment', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 3 AND description = 'Vulnerability Assessment' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 4, 'Authentication', 2 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 4 AND description = 'Authentication' AND flag_id = 2);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 4, 'Traffic Analysis', 2 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 4 AND description = 'Traffic Analysis' AND flag_id = 2);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 4, 'Exposure Protection', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 4 AND description = 'Exposure Protection' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 4, 'Secure Protocols', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 4 AND description = 'Secure Protocols' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 5, 'Access Monitoring', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 5 AND description = 'Access Monitoring' AND flag_id = 1);
+INSERT INTO capability(domain_id, description, flag_id) SELECT 5, 'Log Centralization', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 5 AND description = 'Log Centralization' AND flag_id = 1);
+
+--
+-- DROP TABLE IF EXISTS flag;
+
+CREATE TABLE IF NOT EXISTS flag (
+  id SERIAL PRIMARY KEY,
+  description varchar(128),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO flag(description) SELECT 'red' WHERE NOT EXISTS (SELECT 1 FROM flag where description = 'red');;
+INSERT INTO flag(description) SELECT 'green' WHERE NOT EXISTS (SELECT 1 FROM flag where description = 'green');;
+
+
--- charts/telescope-frontend/templates/_helpers.tpl
+++ charts/telescope-frontend/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "telescope-backend.name" -}}
+{{- define "telescope-frontend.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "telescope-backend.fullname" -}}
+{{- define "telescope-frontend.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "telescope-backend.chart" -}}
+{{- define "telescope-frontend.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "telescope-backend.labels" -}}
+{{- define "telescope-frontend.labels" -}}
 helm.sh/chart: {{ include "postgresql.chart" . }}
-{{ include "telescope-backend.selectorLabels" . }}
+{{ include "telescope-frontend.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,7 +45,7 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "telescope-backend.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "telescope-backend.name" . }}
+{{- define "telescope-frontend.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "telescope-frontend.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
