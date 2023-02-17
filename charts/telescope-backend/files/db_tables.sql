f8639f67403d1315733ddcf6a92d33b968192bb7
Chris Jenkins <chrisj@redhat.com>
2023-02-17 11:09:13 +0000

    Updated db schema to auto increment the ids for integrations

--- charts/telescope-backend/files/db_tables.sql
+++ charts/telescope-backend/files/db_tables.sql
@@ -1,41 +1,206 @@
--- 
--- DROP TABLE IF EXISTS domain;
-CREATE TABLE IF NOT EXISTS domain (
-  id SERIAL PRIMARY KEY,
-  description varchar(128),
-  created_at TIMESTAMP DEFAULT NOW()
+--
+-- PostgreSQL database dump
+--
+
+
+CREATE TABLE public.capability (
+    id integer NOT NULL,
+    domain_id integer,
+    flag_id integer,
+    description character varying(128),
+    created_at timestamp without time zone DEFAULT now()
 );
-INSERT INTO domain(description) SELECT 'Secure Infrastructure' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Infrastructure');
-INSERT INTO domain(description) SELECT 'Secure Data' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Data');
-INSERT INTO domain(description) SELECT 'Secure Code' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Code');
-INSERT INTO domain(description) SELECT 'Secure Integrations' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Integrations');
-INSERT INTO domain(description) SELECT 'Secure Monitoring & Logging' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Monitoring & Logging');
 
--- 
 
--- DROP TABLE integrations
-CREATE TABLE IF NOT EXISTS integrations (
-    integration_id bigint NOT NULL,
+ALTER TABLE public.capability OWNER TO postgres;
+
+--
+-- Name: capability_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.capability_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.capability_id_seq OWNER TO postgres;
+
+--
+-- Name: capability_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.capability_id_seq OWNED BY public.capability.id;
+
+
+--
+-- Name: domain; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.domain (
+    id integer NOT NULL,
+    description character varying(128),
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
+ALTER TABLE public.domain OWNER TO postgres;
+
+--
+-- Name: domain_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.domain_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.domain_id_seq OWNER TO postgres;
+
+--
+-- Name: domain_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.domain_id_seq OWNED BY public.domain.id;
+
+
+--
+-- Name: flag; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.flag (
+    id integer NOT NULL,
+    description character varying(128),
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
+ALTER TABLE public.flag OWNER TO postgres;
+
+--
+-- Name: flag_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.flag_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.flag_id_seq OWNER TO postgres;
+
+--
+-- Name: flag_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.flag_id_seq OWNED BY public.flag.id;
+
+
+--
+-- Name: integration_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.integration_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.integration_id_seq OWNER TO postgres;
+
+--
+-- Name: integrations; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.integrations (
+    integration_id bigint DEFAULT nextval('public.integration_id_seq'::regclass) NOT NULL,
     capability_id integer,
     url character varying(300),
     "user" character varying(100),
     password character varying(100),
     token character varying(5000),
     success_criteria character varying(100),
-    last_update date,
+    last_update timestamp with time zone,
     integration_name character varying(100)
 );
 
 
--- DROP TABLE capability;
+ALTER TABLE public.integrations OWNER TO postgres;
 
-CREATE TABLE IF NOT EXISTS capability (
-  id SERIAL PRIMARY KEY,
-  domain_id int,
-  flag_id int,
-  description varchar(128),
-  created_at TIMESTAMP DEFAULT NOW()
-);
+--
+-- Name: capability id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.capability ALTER COLUMN id SET DEFAULT nextval('public.capability_id_seq'::regclass);
+
+
+--
+-- Name: domain id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.domain ALTER COLUMN id SET DEFAULT nextval('public.domain_id_seq'::regclass);
+
+
+--
+-- Name: flag id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.flag ALTER COLUMN id SET DEFAULT nextval('public.flag_id_seq'::regclass);
+
+
+--
+-- Name: capability capability_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.capability
+    ADD CONSTRAINT capability_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: domain domain_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.domain
+    ADD CONSTRAINT domain_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: flag flag_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.flag
+    ADD CONSTRAINT flag_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: integrations integrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.integrations
+    ADD CONSTRAINT integrations_pkey PRIMARY KEY (integration_id);
+
+
+--
+-- Add in Data
+--
+
+INSERT INTO domain(description) SELECT 'Secure Infrastructure' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Infrastructure');
+INSERT INTO domain(description) SELECT 'Secure Data' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Data');
+INSERT INTO domain(description) SELECT 'Secure Code' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Code');
+INSERT INTO domain(description) SELECT 'Secure Integrations' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Integrations');
+INSERT INTO domain(description) SELECT 'Secure Monitoring & Logging' WHERE NOT EXISTS (SELECT 1 FROM domain where description = 'Secure Monitoring & Logging');
 
 INSERT INTO capability(domain_id, description, flag_id) SELECT 1, 'Platform Hardening', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 1 AND description = 'Secure Infrastructure' AND flag_id = 1);
 INSERT INTO capability(domain_id, description, flag_id) SELECT 1, 'Container Protection',1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 1 AND description = 'Container Protection' AND flag_id = 1);
@@ -54,16 +219,9 @@ INSERT INTO capability(domain_id, description, flag_id) SELECT 4, 'Secure Protoc
 INSERT INTO capability(domain_id, description, flag_id) SELECT 5, 'Access Monitoring', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 5 AND description = 'Access Monitoring' AND flag_id = 1);
 INSERT INTO capability(domain_id, description, flag_id) SELECT 5, 'Log Centralization', 1 WHERE NOT EXISTS (SELECT 1 FROM capability where domain_id = 5 AND description = 'Log Centralization' AND flag_id = 1);
 
---
--- DROP TABLE IF EXISTS flag;
-
-CREATE TABLE IF NOT EXISTS flag (
-  id SERIAL PRIMARY KEY,
-  description varchar(128),
-  created_at TIMESTAMP DEFAULT NOW()
-);
-
 INSERT INTO flag(description) SELECT 'red' WHERE NOT EXISTS (SELECT 1 FROM flag where description = 'red');
 INSERT INTO flag(description) SELECT 'green' WHERE NOT EXISTS (SELECT 1 FROM flag where description = 'green');
+
+
 
 
